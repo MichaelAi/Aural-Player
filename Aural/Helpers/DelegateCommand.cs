0d6c1eec0c57268998cf2382c0b27df5c825a1ed
Michael Ai <michael.ai@outlook.com>
2016-04-06 22:52:52 +0200

    Added Removing multiple items
    now multiple items can be selected and removed from the playlist.

---
+++ Aural/Helpers/DelegateCommand.cs
@@ -0,0 +1,51 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Aural.Helpers
+{
+    public class DelegateCommand<T> : ICommand
+    {
+        
+    #region Constructors      
+    public DelegateCommand(Action<object> execute)
+    : this(execute, null) { }
+
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        #endregion
+
+        #region ICommand Members
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute != null ? _canExecute(parameter) : true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (_execute != null)
+                _execute(parameter);
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        #endregion
+
+        private readonly Action<object> _execute = null;
+        private readonly Predicate<object> _canExecute = null;
+    }
+}
--- Aural/View/PlaylistControl.xaml.cs
+++ Aural/View/PlaylistControl.xaml.cs
@@ -1,8 +1,11 @@
-﻿using Aural.ViewModel;
+﻿using Aural.Converters;
+using Aural.Model;
+using Aural.ViewModel;
 using GalaSoft.MvvmLight.Messaging;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -25,9 +28,12 @@ namespace Aural.View
 {
     public sealed partial class PlaylistControl : UserControl
     {
+        ListView lv = null;
+        ObservableCollection<PlaylistItem> selectedItems = new ObservableCollection<PlaylistItem>();
         public PlaylistControl()
         {
             this.InitializeComponent();
+            lv = PlaylistListView;
         }
 
         private void OnFileDragOver(object sender, DragEventArgs e)
@@ -43,11 +49,11 @@ namespace Aural.View
                 if (items.Count > 0)
                 {
                     List<StorageFile> files = new List<StorageFile>();
-                    foreach (var item in items.OfType<StorageFile>().Select(storageFile => new AppFile { Name = storageFile.Name, File = storageFile}))
+                    foreach (var item in items.OfType<StorageFile>().Select(storageFile => new AppFile { Name = storageFile.Name, File = storageFile }))
                     {
                         files.Add(item.File);
                     }
-                    Messenger.Default.Send<NotificationMessage<IReadOnlyList<StorageFile>>>(new NotificationMessage<IReadOnlyList<StorageFile>>(files,"fromDragDrop"));
+                    Messenger.Default.Send<NotificationMessage<IReadOnlyList<StorageFile>>>(new NotificationMessage<IReadOnlyList<StorageFile>>(files, "fromDragDrop"));
                 }
             }
         }
@@ -58,14 +64,39 @@ namespace Aural.View
             var flyout = (FlyoutBase)uiSender.GetValue(FlyoutBase.AttachedFlyoutProperty);
             flyout.Placement = FlyoutPlacementMode.Bottom;
             flyout.ShowAt(uiSender as FrameworkElement);
+
         }
 
 
-    }
+        private void MenuFlyoutItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (selectedItems.Count == 0)
+            {
+                selectedItems = new ObservableCollection<PlaylistItem>();
+                var x = ((sender as UIElement) as MenuFlyoutItem);
+                selectedItems.Add(x.CommandParameter as PlaylistItem);
+            }
+            var viewModel = (PlaylistViewModel)DataContext;
+            if (viewModel.RemoveItemsFromPlaylistCommand.CanExecute(null))
+                viewModel.RemoveItemsFromPlaylistCommand.Execute(selectedItems);
+        }
 
-    public class AppFile
-    {
-        public string Name { get; set; }
-        public StorageFile File { get; set; }
+        private void Grid_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            //iterating through the list is the only way to retrieve selected items
+            //i've tried a lot of things and this is the only thing that worked
+            selectedItems.Clear();
+            foreach (var item in lv.SelectedItems)
+            {
+                selectedItems.Add(item as PlaylistItem);
+            }
+        }
+
+        public class AppFile
+        {
+            public string Name { get; set; }
+            public StorageFile File { get; set; }
+        }
+
     }
 }
